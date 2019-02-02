0f6d248279c014c578d99d86d32feae828e457d7
seth <liyidong@yeezon.com>
2019-02-02 15:40:51 +0800

    init project

---
+++ app.js
@@ -0,0 +1,9 @@
+const fml = require('./fml');
+
+// fml.top('day').then(data => {
+//   console.log(data);
+// })
+
+fml.random().then(data => {
+  console.log(data);
+})
---
+++ fml.js
@@ -0,0 +1,37 @@
+const request = require("request-promise");
+const load = require("cheerio").load;
+
+exports.random = async() => {
+    const res = await request("http://www.fmylife.com/random");
+    const $ = load(res);
+
+    return $("p.block").text().split("\n\n").filter(x => x.length && x.length !== 0);
+};
+
+exports.search = async(term) => {
+    if (!term) throw new Error("No search term was provided.");
+
+    const res = (await request(`http://www.fmylife.com/search/${encodeURIComponent(term)}`)).body;
+
+    const $ = load(res);
+
+    return $("p.block").text().split("\n\n").filter(x => x.length && x.length !== 0);
+};
+
+exports.top = async(filter) => {
+    const filters = {
+        day: "top_day",
+        week: "top_week",
+        month: "top_month",
+        year: "top_year",
+        allTime: "top"
+    };
+
+    if (!filters[filter]) throw new Error("Invalid filter.");
+
+    const res = await request(`http://www.fmylife.com/tops/top/${filters[filter]}`);
+
+    const $ = load(res);
+
+    return $("p.block").text().split("\n\n").filter(x => x.length && x.length !== 0);
+};
